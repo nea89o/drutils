5c08b6f25d9ff998273c244ca2fee0ae8d04b632
romangraef <romangraef@gmail.com>
2019-09-08 19:48:14 +0200

    oof

--- drutils/__init__.py
+++ drutils/__init__.py
@@ -1,11 +1,11 @@
 from .awaiter import AdvancedAwaiter, AwaitException, AwaitCanceled, AwaitTimedOut
-from .converters import FilteredConverter, non_nullable
+from .converters import AugmentedConverter, non_nullable
 from .eval import handle_eval
 from .version import VERSION, VersionInfo
 
 __all__ = (
     'AdvancedAwaiter', 'AwaitException', 'AwaitCanceled', 'AwaitTimedOut',
-    'FilteredConverter', 'non_nullable',
+    'AugmentedConverter', 'non_nullable',
     'handle_eval',
     'VERSION', 'VersionInfo',
 )
